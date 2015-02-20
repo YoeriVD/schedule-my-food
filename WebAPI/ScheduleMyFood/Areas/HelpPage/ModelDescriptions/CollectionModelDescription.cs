e4117daa3d5dafedf5d73b87fd75ffb252ba5a17
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-02-20 19:25:45 +0100

    add webapi empty project

---
+++ WebAPI/ScheduleMyFood/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ScheduleMyFood.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebAPI/ScheduleMyFood/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ScheduleMyFood.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
