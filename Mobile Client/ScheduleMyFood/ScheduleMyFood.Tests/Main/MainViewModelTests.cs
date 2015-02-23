e5e42772b47173d80efdf256f311bb66c19bfbaf
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-02-23 20:28:30 +0100

    build fix

---
+++ Mobile Client/ScheduleMyFood/ScheduleMyFood.Tests/Main/MainViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ScheduleMyFood.Main;
+using ScheduleMyFood.Tests.IoC;
+
+namespace ScheduleMyFood.Tests.Main
+{
+	[TestFixture ()]
+	public class MainViewModelTests
+	{
+	    private IMainViewModel _sut;
+
+
+	    [TestFixtureSetUp]
+	    public void Initialize()
+	    {
+	        _sut = IoCContainer.CreateTestSubject<IMainViewModel>();
+	    }
+
+		[Test ()]
+		public void MainViewModel_should_contain_a_maint_text ()
+		{
+		    _sut.MainText.Should().Be("Hello!");
+		}
+	}
+}
+
