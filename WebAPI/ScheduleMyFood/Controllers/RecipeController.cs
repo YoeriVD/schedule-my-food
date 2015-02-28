466f7f387e95f9087401252426fa30ab73d8bef8
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-03-01 00:05:16 +0100

    - removed conventional routing and added attribute routing - added test controller for recipes - created shared schema

---
+++ WebAPI/ScheduleMyFood/Controllers/RecipeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using SharedSchema;
+
+namespace ScheduleMyFood.Controllers
+{
+    [RoutePrefix("recipes")]
+    public class RecipeController : ApiController
+    {
+        private static readonly List<Recipe> _recipes = new List<Recipe>
+            {
+                new Recipe() { Name = "Lasagna"},
+                new Recipe() { Name = "Croque monsieur"},
+                new Recipe() { Name = "WAP"},
+                new Recipe() { Name = "Puree"},
+                new Recipe() { Name = "Waterzooi"},
+                new Recipe() { Name = "Frieten met stoofvlees"},
+                new Recipe() { Name = "Tonijn"},
+                new Recipe() { Name = "Kabeljouw"},
+                new Recipe() { Name = "Rijst"}
+            };
+        [Route("")]
+        public IEnumerable<Recipe> Get()
+        {
+            return _recipes;
+        }
+        [Route("{name}")]
+        public IHttpActionResult Get(string name)
+        {
+            var recipe = _recipes.SingleOrDefault(rec => rec.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            return Ok(recipe);
+        }
+        //[Authorize]
+        [Route("")]
+        public IHttpActionResult Post(Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                if(_recipes.Any(rec => rec.Name.Equals(recipe.Name, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    return BadRequest("a recipe with this name already exists");
+                }
+                _recipes.Add(recipe);
+                return Created("api/recipes/" + recipe.Name, recipe);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
