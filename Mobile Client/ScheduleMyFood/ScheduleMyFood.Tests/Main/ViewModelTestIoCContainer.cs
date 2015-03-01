ebebda19aa81eebee20a30f8b4ea599f856c1d51
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-03-01 04:04:24 +0100

    added view for data and check in of previously forgotten files

---
+++ Mobile Client/ScheduleMyFood/ScheduleMyFood.Tests/Main/ViewModelTestIoCContainer.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Autofac.Core;
+
+namespace ScheduleMyFood.Tests.Main
+{
+    class ViewModelTestIoCContainer
+    {
+        private static readonly IContainer Container;
+
+        static ViewModelTestIoCContainer()
+        {
+            var builder = new ContainerBuilder();
+            // Registers all modules
+            builder
+                .AddServiceMocks()
+                .RegisterAssemblyTypes(typeof(App).Assembly)
+                .Where(t => t.Name.EndsWith("ViewModel"))
+                .AsImplementedInterfaces();
+            Container = builder.Build();
+        }
+
+        public static T CreateTestSubject<T>(params Parameter[] parameters)
+        {
+            T sut;
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                sut = scope.Resolve<T>(parameters);
+            }
+            return sut;
+        }
+    }
+
+    internal static class BuilderExtensions
+    {
+        internal static ContainerBuilder AddServiceMocks(this ContainerBuilder builder)
+        {
+            builder.RegisterInstance(ServiceMocks.RecipeProxy.Object);
+            return builder;
+        }
+    }
+}
---
+++ Mobile Client/ScheduleMyFood/ScheduleMyFood.Tests/ServiceMocks.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using ScheduleMyFood.Proxies;
+using ScheduleMyFood.Technical;
+
+namespace ScheduleMyFood.Tests
+{
+    static class ServiceMocks
+    {
+        internal static Mock<IRecipeProxy> RecipeProxy { get; private set; }
+        internal static Mock<IFoodHttpClient> FoodHttpClient { get; private set; }
+
+        static ServiceMocks()
+        {
+            RecipeProxy = new Mock<IRecipeProxy>();
+            FoodHttpClient = new Mock<IFoodHttpClient>();
+        }
+    }
+}
