3b75a0a71c29952cc8821ac22581484144456a9a
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-03-04 00:15:15 +0100

    added missing files

---
+++ Mobile Client/ScheduleMyFood/ScheduleMyFood/Recipes/RecipePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ScheduleMyFood.Annotations;
+using Xamarin.Forms;
+
+namespace ScheduleMyFood.Recipes
+{
+    public partial class RecipePage : ContentPage
+    {
+        private readonly IMainViewModel _mainViewModel;
+
+        public RecipePage([NotNull] IMainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            if (mainViewModel == null) throw new ArgumentNullException(nameof(mainViewModel));
+            InitializeComponent();
+        }
+
+        internal RecipePage Activate()
+        {
+            this.BindingContext =_mainViewModel;
+            return this;
+        }
+    }
+}
---
+++ Mobile Client/ScheduleMyFood/ScheduleMyFood/Technical/HttpClientExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ScheduleMyFood.Technical
+{
+    static class HttpClientExtensions
+    {
+        public static async Task<T> GetAsync<T>(this HttpClient client,string resourceName) where T : new()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, resourceName);
+            return await client.SendRequest<T>(request);
+        }
+        public static async Task<TResponse> PostAsync<TRequest, TResponse>(this HttpClient client, string resourceName, TRequest resource) where TResponse : new()
+        {
+            var resourceAsString = JsonConvert.SerializeObject(resource);
+            var request = new HttpRequestMessage(HttpMethod.Post, resourceName)
+            {
+                Content = new StringContent(resourceAsString, Encoding.UTF8, App.Constants.ApplicationJson)
+            };
+            return await client.SendRequest<TResponse>(request);
+        }
+        public static async Task<T> PostAsync<T>(this HttpClient client,string resourceName, T resource) where T : new()
+        {
+            return await client.PostAsync<T, T>(resourceName, resource);
+        }
+        internal static async Task<T> SendRequest<T>(this HttpClient client, HttpRequestMessage request) where T : new()
+        {
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var value = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+        internal static void SetAuthenticationToken(this HttpClient client, string accessToken)
+        {
+            client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+        }
+    }
+}
